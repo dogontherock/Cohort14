6bdd487d590c19e5405123447b6df9405c4b7f14
scambler <scambler@hotmail.com>
2024-11-19 08:13:36 +0000

    Add files via upload

---
+++ week3/section3_part2.py
@@ -0,0 +1,48 @@
+"""
+Prompts the user for a weight on Earth
+and a planet (in separate inputs). Then 
+prints the equivalent weight on that planet.
+
+Note that the user should type in a planet with 
+the first letter as uppercase, and you do not need
+to handle the case where a user types in something 
+other than one of the planets (that is not Earth). 
+"""
+
+# Instructions
+# Planetary Weight Calculator
+# Mars is not the only planet in our solar system with its own unique gravity. In fact, each planet has a different gravitational constant, which affects how much an object would weigh on that planet. Below is a list of the constants for each planet compared to Earth's gravity:
+# - Mercury: 37.6%
+# - Venus: 88.9%
+# - Mars: 37.8%
+# - Jupiter: 236.0%
+# - Saturn: 108.1%
+# - Uranus: 81.5%
+# - Neptune: 114.0%
+
+# Write a Python program that prompts an Earthling to enter their weight on Earth and then to enter the name of a planet in our solar system. The program should then use if statements to find the correct gravitational constant for the selected planet and calculate the person's weight on that planet. Finally, the program should print the calculated weight, rounded to 2 decimal places. 
+
+# Sample Run
+# $ python planetaryweight.py
+
+# Enter a weight on Earth: 120
+
+# Enter a planet: Mars
+
+# The equivalent weight on Mars: 45.36
+
+# Sample Run
+# $ python planetaryweight.py
+
+# Enter a weight on Earth: 150
+
+# Enter a planet: Jupiter
+
+# The equivalent weight on Jupiter: 354.0
+
+def main():
+    # Fill this function out!
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ week3/section_3_part1.py
@@ -0,0 +1,23 @@
+"""
+Prompts the user for a weight on Earth
+and prints the equivalent weight on Mars.
+"""
+
+
+# Sample Run
+
+# $ python marsweight.py
+
+# Enter a weight on Earth: 120
+
+# The equivalent weight on Mars: 45.36
+
+
+
+
+def main():
+    # Fill this function out!
+    pass
+
+if __name__ == "__main__":
+    main()
